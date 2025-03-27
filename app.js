d8be6e2e5b5989a00b968354c79216984ca21b1c
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-03-27 17:19:49 +0100

    feat: crud operation

--- app.js
+++ app.js
@@ -6,22 +6,61 @@ const posts = require('./data/posts');
 
 app.use(express.static('public'));
 
+
+// HOME
+
 app.get('/', (req, res) => {
     res.send('Benvenuto nel mio blog!');
     console.log('Benvenuto nel mio blog!');
 });
 
+
+// INDEX
+
 app.get('/posts', (req, res) => {
-    res.send('Ecco i miei posts!');
+    res.send(posts);
     console.log('Ecco i miei posts!');
 });
+
+
+// SHOW
 
 app.get('/posts/:id', (req, res) => {
     const currentId = req.params.id;
     const currentPost = posts[Number(currentId)].title;
-    res.send(currentPost);
-    console.log(req.params.id);
+    res.send('Ecco il post:' + currentPost);
+    console.log('Ecco il post:' + currentPost);
 });
+
+
+
+// CREATE
+
+app.post('/posts', (req, res) => {
+    res.send('Creazione nuovo post!');
+    console.log('Creazione nuovo post!');
+});
+
+
+
+// UPDATE
+
+app.put('/posts/:id', (req, res) => {
+    const currentId = req.params.id;
+    res.send('Modifica del post ' + currentId + '!');
+    console.log('Modifica del post ' + currentId + '!');
+});
+
+
+
+// DELETE
+
+app.delete('/posts/:id', (req, res) => {
+    const currentId = req.params.id;
+    res.send('Cancellazione del post ' + currentId + '!');
+    console.log('Cancellazione del post ' + currentId + '!');
+});
+
 
 
 app.listen(port, () => {
